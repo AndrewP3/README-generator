0bdc440119211357308a95d2fb03509eae801070
AndrewP3 <andrewpowell097@gmail.com>
2021-06-15 20:36:01 -0700

    Second commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,75 @@
-// TODO: Include packages needed for this application
+// Dependacies and variables
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please provide a project title. (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username. (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your Github username');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Please enter a name for repo. (Required)',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please enter a name for repo');
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description for application. (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please provide a description for application');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usuage',
+        message: 'Please provide info for using the application. (Required)',
+        validate: usuageInput => {
+            if (usuageInput) {
+                return true;
+            } else {
+                console.log('Please provide info for using the application');
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
