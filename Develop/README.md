78cd061d560b435a45243cd71eb91463343a3759
AndrewP3 <andrewpowell097@gmail.com>
2021-06-18 20:45:02 -0700

    Third commit

---
+++ Develop/README.md
@@ -0,0 +1,11 @@
+# sdfghn
+
+## description
+
+sdfghn
+
+
+## license
+
+The following link contains in depth information about license selected: 
+    https://choosealicense.com/licenses/agpl-3.0
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,16 +4,39 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  console.log("this is license link"+license)
+  if (license) {
+    return `
+    https://choosealicense.com/licenses/${license}`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  console.log("this is license section"+license)
+  if (license) {
+    return `
+## license
+
+The following link contains in depth information about license selected: ${renderLicenseLink(license)}`;
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data.license)
   return `# ${data.title}
 
+## description
+
+${data.description}
+
+${renderLicenseSection(data.license)}
 `;
 }
 
